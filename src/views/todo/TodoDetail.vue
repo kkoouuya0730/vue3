2f0111604502234b24394b707bd89f5b2648ffe6
kkoouuya0730 <kouya.0730@icloud.com>
2023-05-31 21:52:14 +0900

    :white_check_mark: today's output

---
+++ src/views/todo/TodoDetail.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router"
+
+
+</script>
+<template>
+    <h1>Todo管理</h1>
+    <nav id="breadcrumbs">
+        <ul>
+            <li>
+                <RouterLink v-bind:to="{name: 'AppTop'}">
+                    TOP
+                </RouterLink>
+            </li>
+            <li>
+                <RouterLink v-bind:to="{name: 'TodoList'}">
+                    Todoリスト
+                </RouterLink>
+            </li>
+            <li>Todo詳細情報</li>
+        </ul>
+    </nav>
+    <section>
+        <h2>Todo詳細情報</h2>
+    </section>
+</template>
